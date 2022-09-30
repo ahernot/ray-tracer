1bbf166dcf9e19f24f031b17fd1d833ddaa510d8
ahernot <70817470+ahernot@users.noreply.github.com>
2022-09-30 20:52:11 +0200

    improve sim structure

--- sim.py
+++ sim.py
@@ -30,22 +30,44 @@ class Environment2D:
 
 
 
-
 class Ray2D:
 
-    def __init__ (self, env: Environment2D):
-        self.env = env
+    def __init__ (self, env: Environment2D, angle):
+        self.__env = env
+        self.__angle = angle
 
-    def propagate (self):
+    def propagate (self):  # propagation params
         pass
+
 
 
 class Simulation2D:
 
     def __init__ (self, env: Environment2D):
         self.__env = env
-        self.__rays = list()
+        self.__rays = list()  # Raw list of rays (stored as references to Ray2D objects)
+        self.__angles = dict()  # Dictionary of Ray2D objects grouped by angle
+        # can group rays by frequency or some other key if needed
+        self.n_rays = 0
+        self.n_angles = 0
 
-    def add_ray (self):
+    def __repr__ (self):
+        return f'2D simulation containing {self.n_rays} rays'  # TODO: to improve later
+
+    def add_rays (self, *angles):
+        # Supports two rays for the same angle (for example two different frequencies)
+
+        for angle in angles:
+            # Generate ray
+            ray = Ray2D (env=self.__env, angle=angle)
+            ray.propagate()  # feed propagation params for the simulation
+
+            # Add ray to simulation
+            self.__rays .append(ray)
+            self.__angles[angle] = self.__angles[angle] + [ray] if angle in self.__angles else [ray]
+        
+        self.n_rays = len(self.__rays)
+        self.n_angles = len(self.__angles)
+
+    def plot (self):
         pass
-
