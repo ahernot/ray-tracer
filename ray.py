4fb64b223f7320e61bd7bb072469e92c1fe6bd0e
ahernot <70817470+ahernot@users.noreply.github.com>
2022-09-30 20:26:44 +0200

    Update ray.py

--- ray.py
+++ ray.py
@@ -1,8 +1,48 @@
 
 
-class Ray:
 
-    def __init__ (self):
+
+class Point2D:
+
+    def __init__ (self, x, z):
+        self.__x = x
+        self.__z = z
+
+    def __repr__ (self):
+        return f'2D point at coordinates ({self.__x}, {self.__z})'
+    
+    def coordinates (self):
+        return self.__x, self.__z
+
+
+
+
+class Environment:
+
+    def __init__ (self, source: Point2D, floor, ceiling):
+        self.__source = source
+
+    def __generate_normals (self):
         pass
 
-    
+# class EnvironmentStatic (Environment):
+#     def __init__ (self, source: Point2D, floor, ceiling):
+#         pass
+
+
+class Ray:
+
+    def __init__ (self, env: Environment):
+        self.__env = env
+
+    def propagate (self):
+        pass
+
+
+class Simulation:
+
+    def __init__ (self, env: Environment, *rays: Ray):
+        self.__rays = rays  # ordered by angle??
+        self.__env = env
+
+    pass
