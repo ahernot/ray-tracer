b8159dd036eac257094148eb9c0c8d411dc9ba00
ahernot <70817470+ahernot@users.noreply.github.com>
2022-10-01 15:29:52 +0200

    prepare ray

--- environment.py
+++ environment.py
@@ -5,6 +5,8 @@ class Environment2D:
 
     def __init__ (self, source: Point2D, floor, ceiling):
         self.__source = source
+        # function to model ray speed as a function of x and z
+        # current as additional displacement (vector field)
 
     def __generate_normals (self):
         pass
--- ray.py
+++ ray.py
@@ -6,5 +6,18 @@ class Ray2D:
         self.__env = env
         self.__angle = angle
 
+    def __repr__ (self):
+        return f'Ray object'  # TODO: improve repr
+
     def propagate (self):  # propagation params
+        
+        # Minimum resolutions
+        dx_max = 0.1
+        dz_max = 0.1
+        # adaptive dz based on angle
+
+
+
+
+        
         pass
