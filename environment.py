b8cc66f7cbea2c9b7ddaeedf657235e4dd6ad0c4
ahernot <70817470+ahernot@users.noreply.github.com>
2022-10-03 14:00:38 +0200

    add simulation range

--- environment.py
+++ environment.py
@@ -1,12 +1,19 @@
-from geometry import Point2D
+from geometry import Point2D, Vector2D
 
 
 class Environment2D:
 
-    def __init__ (self, source: Point2D, floor, ceiling):
-        self.__source = source
+    def __init__ (self, floor, ceiling):
+        # self.__source = source
         # function to model ray speed as a function of x and z
         # current as additional displacement (vector field)
+
+        #self.range = {'x': (0, 100000), 'z': (-10000, 10000)}
+        self.range_min = Vector2D (0, -10000)
+        self.range_max = Vector2D (100000, 10000)
+
+
+        # reflection power coefficient as a function of distance
 
     def __generate_normals (self):
         pass
