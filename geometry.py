d475019c6b67f24cc418915a36c7710c0cf063e5
ahernot <70817470+ahernot@users.noreply.github.com>
2022-10-04 16:55:21 +0200

    Update geometry.py

--- geometry.py
+++ geometry.py
@@ -23,3 +23,12 @@ class Vector2D:
     
     def coordinates (self):
         return self.x, self.z
+
+
+
+def ccw (A: Point2D, B: Point2D, C: Point2D):
+    return (C.z-A.z) * (B.x-A.x) > (B.z-A.z) * (C.x-A.x)
+
+# Return true if line segments AB and CD intersect
+def intersect (A: Point2D, B: Point2D, C: Point2D, D: Point2D):
+    return ccw(A, C, D) != ccw(B, C, D) and ccw(A, B, C) != ccw(A, B, D)
