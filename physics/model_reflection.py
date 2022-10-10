814fc2b3ca6dfd218bd04fdb2fe209ebb222bca5
ahernot <70817470+ahernot@users.noreply.github.com>
2022-10-10 20:22:29 +0200

    Update model_reflection.py

--- physics/model_reflection.py
+++ physics/model_reflection.py
@@ -4,8 +4,11 @@
 import numpy as np
 
 
-def calc_refcoef_surface (wavelength, wave_height, grazing_angle):
+def calc_refcoef_surface (wavelength, angle, wave_height_rms):
     """
     Coherent rough surface reflection coefficient
+    :param wavelength: Ray wavelength
+    :param angle: grazing angle
+    :param wave_height_rms: RMS wave height
     """
-    return np.exp (-2 * np.power(2 * np.pi / wavelength * wave_height * np.sin(grazing_angle), 2))
+    return np.exp (-2 * np.power(2 * np.pi / wavelength * wave_height_rms * np.sin(angle), 2))
