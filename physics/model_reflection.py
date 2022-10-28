2fdc6e6959f4c78235ef0cafbf5325e2ed5608bc
ahernot <70817470+ahernot@users.noreply.github.com>
2022-10-28 23:07:28 +0200

    Update model_reflection.py

--- physics/model_reflection.py
+++ physics/model_reflection.py
@@ -4,10 +4,11 @@
 
 import numpy as np
 
+from preferences import *
 from physics.profile_impedance import Zp1_DEFAULT, Zp2_DEFAULT
 
 
-def calc_refcoef_surface (wavelength, angle, wave_height_rms):
+def calc_refcoef_surface (wavelength, angle, wave_height_rms = WAVE_HEIGHT_RMS_DEFAULT):
     """
     Coherent rough surface reflection coefficient
     :param wavelength: Ray wavelength
@@ -17,7 +18,7 @@ def calc_refcoef_surface (wavelength, angle, wave_height_rms):
     return np.exp (-2 * np.power(2 * np.pi / wavelength * wave_height_rms * np.sin(angle), 2))
 
 
-def calc_refcoef_sediment (angle, Zp0, Zp1 = Zp1_DEFAULT, Zp2 = Zp2_DEFAULT, dz_sediment = 2):
+def calc_refcoef_sediment (angle, Zp0, Zp1 = Zp1_DEFAULT, Zp2 = Zp2_DEFAULT, dz_sediment = DZ_SEDIMENT_DEFAULT):
     """
     
     :param angle: Grazing angle (in radians) - complementary to the incident angle
