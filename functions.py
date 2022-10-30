b6488cc4c89eff8d41ee60e2caf66766fcaad2c4
ahernot <70817470+ahernot@users.noreply.github.com>
2022-10-30 15:54:48 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,11 +1,12 @@
 import numpy as np
 
-def coords_to_mask_2d (shape, coords):
-    mask_blank = np.zeros(shape, dtype=bool)
+def coords_to_mask_2d (shape, coords, vals=True):
+    mask_blank = np.zeros(shape)
     mask_line = mask_blank.reshape(-1)
 
     c_line = coords[:, 0] * shape[0] + coords[:, 1]
     c_line_filtered = c_line[c_line < mask_line.shape[0]]
 
-    mask_line[c_line_filtered] = True
+    mask_line[c_line_filtered] = vals
     return mask_line.reshape(shape[::-1]).T
+
