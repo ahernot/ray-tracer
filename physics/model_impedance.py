3acd8de7b20f1951e134f43e7e903121609b1363
ahernot <70817470+ahernot@users.noreply.github.com>
2022-11-04 15:48:19 +0100

    Update model_impedance.py

--- physics/model_impedance.py
+++ physics/model_impedance.py
@@ -9,10 +9,10 @@ from physics.profile_temperature import calc_T
 from physics.profile_pressure import calc_P
 
 
-def calc_Z (z):
-    c = calc_c(z)
-    rho = calc_rho(z, calc_S(z), calc_T(z), calc_P(z))  # TODO: put all the calc_() functions in a class so they can all access each other's z etc
-    return c * rho
+# def calc_Z (z):
+#     c = calc_c(z)
+#     rho = calc_rho(z, calc_S(z), calc_T(z), calc_P(z))  # TODO: put all the calc_() functions in a class so they can all access each other's z etc
+#     return c * rho
 
-# def calc_Z (rho, c) :
-#     return rho * c
+def calc_Z (rho, c) :
+    return rho * c
