fa20fc2e09c55dcca040b9a934e11fb2eb026596
ahernot <70817470+ahernot@users.noreply.github.com>
2022-11-04 15:29:22 +0100

    implement PhysicsEnvironment2D

--- environment.py
+++ environment.py
@@ -3,6 +3,12 @@ import matplotlib.pyplot as plt
 
 from scipy import interpolate
 
+from physics_environment import PhysicsEnvironment2D
+
+
+RES_X_DEFAULT = 100
+RES_Z_DEFAULT = 10
+
 
 class Environment2D:
 
@@ -11,32 +17,33 @@ class Environment2D:
         # function to model ray speed as a function of x and z
         # current as additional displacement (vector field)
 
-        # TODO: Use discrete matrices instead (set resolution) for x,t derivation and time-moving limits (floor, …)
+        # Initialise range and resolution
+        self.res_x = kwargs.get('res_x', RES_X_DEFAULT)
+        self.res_z = kwargs.get('res_z', RES_Z_DEFAULT)
 
-        #self.range = {'x': (0, 100000), 'z': (-10000, 10000)}
         self.range_min = np.array([0, -5500])
         self.range_max = np.array([100000, 0])
         self.size = self.range_max - self.range_min
 
+        # Generate physics environment
+        self.penv = PhysicsEnvironment2D(self.range_min, self.range_max, res_x=RES_X_DEFAULT, res_z=RES_Z_DEFAULT)
+
         self.floor = floor
         self.ceil = ceiling
+        self.__floor_sampled = self.floor(self.penv.x)
+        self.__ceil_sampled = self.ceil(self.penv.x)
 
-        self.__res = kwargs.get('res', 10000)
-        mult = self.__res / self.range_max[0]
-
-        self.__x = np.linspace(self.range_min[0], self.range_max[0], self.__res)  # x resolution across environment  # TODO: use PhysicsEnvironment2D.x
-        self.__floor_sampled = self.floor(self.__x)
-        self.__ceil_sampled = self.ceil(self.__x)
-
-        self.dx_floor = interpolate.interp1d(self.__x, np.gradient(self.__floor_sampled) * mult, kind='quadratic')  # floor is static
-        self.dx_ceil = interpolate.interp1d(self.__x, np.gradient(self.__ceil_sampled) * mult, kind='quadratic')  # ceiling is static
+        self.__dx_floor_sampled = np.gradient(self.__floor_sampled) / self.res_x
+        self.__dx_ceil_sampled = np.gradient(self.__ceil_sampled) / self.res_x
+        self.dx_floor = interpolate.interp1d(self.penv.x, self.__dx_floor_sampled, kind='quadratic')  # floor is static
+        self.dx_ceil = interpolate.interp1d(self.penv.x, self.__dx_ceil_sampled, kind='quadratic')  # ceiling is static
 
         # reflection power coefficient as a function of distance
 
 
     def plot (self, fig, **kwargs):
-        plt.plot(self.__x, self.__ceil_sampled, figure=fig, **kwargs)
-        plt.plot(self.__x, self.__floor_sampled, figure=fig, **kwargs)
+        plt.plot(self.penv.x, self.__ceil_sampled, figure=fig, **kwargs)
+        plt.plot(self.penv.x, self.__floor_sampled, figure=fig, **kwargs)
 
 
 
