9bb02bed586eeab849cf29756bb255e4bb51de62
ahernot <70817470+ahernot@users.noreply.github.com>
2022-11-06 17:50:56 +0100

    refine, add copyright

--- topography/bathymetry.py
+++ topography/bathymetry.py
@@ -1,13 +1,26 @@
+# Bathymetry (v1.0)
+# Copyright Anatole Hernot (Mines Paris), 2022. All rights reserved.
+
 import numpy as np
+
+
+EARTH_RADIUS_AVG = 6378000  # in meters (https://en.wikipedia.org/wiki/Earth_radius)
 
 
 class Map:
 
-    def __init__ (self, map_array, xllcorner, yllcorner, cellsize):
+    def __init__ (self, map, xllcorner, yllcorner, cellsize):
+        """
+        :param map:
+        :param xllcorner: Lower-left corner longitude  # TODO: rename to ll_long
+        :param yllcorner: Lower-left corner latitude  # TODO: rename to ll_lat
+        :param cellsize: Cell size (in degrees)
+        """
         self.xllcorner = xllcorner
         self.yllcorner = yllcorner
         self.cellsize = cellsize
-        self.map = map_array
+        self.res = EARTH_RADIUS_AVG * self.cellsize / 180 * np.pi  # Map resolution (in meters)
+        self.map = map
 
     @classmethod
     def from_asc (cls, path: str):
@@ -26,4 +39,3 @@ class Map:
         map[map==header['NODATA_VALUE']] = np.nan
 
         return cls(map, header['XLLCORNER'], header['YLLCORNER'], header['CELLSIZE'])
-        
