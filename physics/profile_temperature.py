96fdefe1ec69e6f45d21c26852c8557d5e37cca3
ahernot <70817470+ahernot@users.noreply.github.com>
2022-11-23 12:08:49 +0100

    add comment

--- physics/profile_temperature.py
+++ physics/profile_temperature.py
@@ -1,7 +1,7 @@
 # profile_temperature v1.0
 # Copyright Anatole Hernot (Mines Paris), 2022. All rights reserved.
 
-# NO MODEL
+# NO MODEL  # TODO: Quadratic or linear?
 
 import numpy as np
 from scipy import interpolate
